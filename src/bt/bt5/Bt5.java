6c07081dd5c1fd1d2b923746f271eb47eb864271
Anizx9171 <anizx9171@gmail.com>
2023-10-12 11:54:04 +0700

    upp

--- src/bt/bt5/Bt5.java
+++ src/bt/bt5/Bt5.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 public class Bt5 {
     public static void main(String[] args) {
-        String str = "Xin chào mọi người";
-        List<String> newList = new ArrayList<>(Arrays.asList(str.split(" ")));
+        String str = "    Xin    chào mọi người";
+        List<String> newList = new ArrayList<>(Arrays.asList(str.trim().split(" ")));
         String stringMinLength = newList.get(0);
         for (String s : newList) {
-            if (s.length() < stringMinLength.length()){
-                stringMinLength = s;
+            if (!s.isEmpty()){
+                if (s.length() < stringMinLength.length()){
+                    stringMinLength = s;
+                }
             }
         }
         System.out.println("Chữ có độ dài nhỏ nhất trong chuỗi là: " + stringMinLength);
